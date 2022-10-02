c6a15b030ce0471505d1d363d3a06b40d304b6b1
Vi7ality <104318952+Vi7ality@users.noreply.github.com>
2022-10-02 20:47:32 +0000

    Deploying to gh-pages from @ Vi7ality/goit-js-hw-09@90bf88afd26e67d50608a22146620bd783b2961b 🚀

---
+++ 01-color-switcher.64b5cf9d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=null;e.addEventListener("click",(function(a){a.preventDefault(),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,t.disabled=!0})),t.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.64b5cf9d.js.map
